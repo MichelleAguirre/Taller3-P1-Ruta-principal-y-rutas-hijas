6d63814a23fa1c416231f9eb76a4b3698f21e3af
[Michelle Aguirre]git configgit helpgit config --global user.name [Michelle <[mkp.aguirre@yavirac.edu.ec]>
2022-11-30 21:42:00 -0500

    Michelle Aguirre

---
+++ src/app/pages/product/product.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+/*   constructor(private httpClient: HttpClient) {
+    this.httpClient.get('https//:api.escuelajs.co/api/v1/products');
+  }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+
+    const response = this.httpClient.get('https//:api.escuelajs.co/api/v1/products');
+    console.log(response);
+  } */
+
+}
